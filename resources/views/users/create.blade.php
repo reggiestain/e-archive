0606cbc3d25fe77a95d54fb8b73abb875c3e0bd1
dev5 <reggiestain@gmail.com>
2020-04-08 00:28:12 +0200

    Updated add user screen

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,11 +1,33 @@
 @extends('layouts.app')
 @section('title','New User')
 @section('content')
-    <section class="content-header">
-        <h1>
-            User
-        </h1>
-    </section>
+<div class="main-content">
+    <div class="container-fluid">
+        <div class="page-header">
+            <div class="row align-items-end">
+                <div class="col-lg-12" style="margin-bottom:20px">
+                    <div class="page-header-title">
+                        <i class="fa fa-tags bg-blue"></i>
+                        <div class="d-inline">
+                            <h5>User</h5>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-header-left">
+                            <h5>
+                                User
+                            </h5>
+                        </div>
+                    </div>            
+            <div class="card-body">
     <div class="content">
 
         {!! Form::open(['route' => 'users.store']) !!}
@@ -14,4 +36,10 @@
 
         {!! Form::close() !!}
     </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 @endsection
